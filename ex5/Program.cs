1d3991e52d48c7548b61744c88d7d0dd7f9a1e5e
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-08-13 18:56:54 +0500

    Добавлены переменные

--- ex5/Program.cs
+++ ex5/Program.cs
@@ -10,6 +10,22 @@ namespace ex5
     {
         static void Main(string[] args)
         {
+            int crystalPrice = 8;
+            bool isPurchased;
+
+            Console.WriteLine($"Добро подаловать в магазин кристаллов! Только у нас продаются " +
+                $"кристаллы по цене {crystalPrice} золотых монет.");
+            Console.Write($"Какое кол-во золотых монет в вашем распоряжении? ");
+            int gold = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Сколько вы хотите приобрести кристаллов? ");
+            int crystals = Convert.ToInt32(Console.ReadLine());
+
+            isPurchased = gold >= crystalPrice * crystals;
+            crystals *= Convert.ToInt32(isPurchased);
+            gold -= crystalPrice * crystals;
+
+            Console.WriteLine($"Теперь у вас {gold} золотых монет и {crystals} кристаллов.");
+            
 
         }
     }
